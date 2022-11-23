8c7f98c69c4dfaf1b1c3a13a6972eaccc28b33c1
italo <italo.holanda@escolaconquer.com.br>
2022-11-23 19:52:40 -0300

    test: test with another class impl

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,48 @@
 import DataStructures.HashTable;
 
-import java.util.NoSuchElementException;
+class User {
+    private String name;
+    private int age;
+
+    public User (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        HashTable<String> hashTable = new HashTable<String>(16);
-        hashTable.addValue("Macacos", "Macacos");
-        hashTable.addValue("Macacos", "Macacos");
+        HashTable<User> hashTable = new HashTable<>(16);
+
+        User robert = new User("robert");
+        User john = new User("john");
+        User ana = new User("ana");
+
+        hashTable.addValue(robert, robert.getName());
+        hashTable.addValue(john, john.getName());
 
         try {
-            System.out.println(hashTable.getValue("Macacos"));
-            System.out.println(hashTable.deleteValue("Macacos"));
-            System.out.println(hashTable.getValue("Macacos"));
+            System.out.println(hashTable.getValue("robert").getName());
+            System.out.println(hashTable.getValue("john").getName());
+            hashTable.updateValue(ana, "john");
+            System.out.println(hashTable.getValue("john").getName());
+            hashTable.deleteValue("john");
+            System.out.println(hashTable.getValue("john").getName());
         } catch (NullPointerException nee) {
             System.out.println("Warning: Tried to access a null value on HashTable");
         } catch (IndexOutOfBoundsException ior) {
